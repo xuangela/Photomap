1191d6c1adfa4c25ee67bdbe6198431a60af4a12
Angela Xu <xuangela@fb.com>
2020-07-08 09:30:52 -0700

    can view map

---
+++ PhotoMap/View Controllers/PhotoMapViewController.m
@@ -0,0 +1,42 @@
+//
+//  PhotoMapViewController.m
+//  PhotoMap
+//
+//  Created by emersonmalca on 7/8/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+#import "PhotoMapViewController.h"
+#import <MapKit/MapKit.h>
+
+@interface PhotoMapViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapview;
+
+@end
+
+@implementation PhotoMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
+    [self.mapview setRegion:sfRegion animated:false];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
