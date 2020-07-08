62146c8b2e3678160a7ff5eac19125ecaafd3177
Angela Xu <xuangela@fb.com>
2020-07-08 09:43:51 -0700

    can take a photo

--- PhotoMap/View Controllers/PhotoMapViewController.m
+++ PhotoMap/View Controllers/PhotoMapViewController.m
@@ -9,9 +9,13 @@
 #import "PhotoMapViewController.h"
 #import <MapKit/MapKit.h>
 
-@interface PhotoMapViewController ()
+@interface PhotoMapViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapview;
+@property (weak, nonatomic) IBOutlet UIButton *cameraButton;
+
+@property (nonatomic, strong)UIImagePickerController *imagePickerVC;
+@property (nonatomic, strong) UIImage *picture;
 
 @end
 
@@ -19,14 +23,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    //setting map view to LA
     MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
     [self.mapview setRegion:sfRegion animated:false];
+    
+    //setting up camera view
+    self.imagePickerVC = [UIImagePickerController new];
+    self.imagePickerVC.delegate = self;
+    self.imagePickerVC.allowsEditing = YES;
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+           self.imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+       }
+       else {
+           NSLog(@"Camera 🚫 available so we will use photo library instead");
+           self.imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+       }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)clickCamera:(id)sender {
+    [self presentViewController:self.imagePickerVC animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    self.picture = editedImage;
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    [self performSegueWithIdentifier:@"tagSegue" sender:nil];
 }
 
 /*
